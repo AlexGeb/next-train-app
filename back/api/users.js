a3e82adbe15038df993f009410d9c42d0751d2b2
Alexandre Behaghel <alex.behaghel@gmail.com>
2019-01-04 09:34:43 +0100

    Users routes

--- back/api/users.js
+++ back/api/users.js
@@ -1,7 +1,7 @@
 const { parse } = require("url");
 const { getDb } = require("../db");
 
-module.exports = (req, res) => {
+const getUsers = (req, res) => {
   const { query } = parse(req.url, true);
   const { name } = query;
   getDb(db => {
@@ -13,4 +13,36 @@ module.exports = (req, res) => {
         res.end(JSON.stringify(users));
       });
   });
+};
+
+const postUser = (req, res) => {
+  const { query } = parse(req.url, true);
+  const { name } = query;
+  getDb(db => {
+    const mongodbQuery = name ? { name } : {};
+    db.collection("users")
+      .find(mongodbQuery)
+      .toArray((err, users) => {
+        if (err) throw err;
+        res.end(JSON.stringify(users));
+      });
+  });
+};
+
+module.exports = (req, res) => {
+  const { method } = req;
+  switch (method) {
+    case "GET":
+      getUsers(req, res);
+      break;
+    case "POST":
+      postUser(req, res);
+      break;
+    // case "DELETE":
+    //   deleteUser(req, res);
+    //   break;
+    default:
+      req.end();
+      break;
+  }
 };
